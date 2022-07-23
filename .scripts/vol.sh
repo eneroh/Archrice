c35488ffcf1c370b1fe7d23b72410177b0a5ce4d
eneroh <28052446+eneroh@users.noreply.github.com>
2022-07-24 02:57:16 +1000

    Update vol.sh

--- .scripts/vol.sh
+++ .scripts/vol.sh
@@ -3,7 +3,7 @@
 # Script isn't very snappy, there is lag on notifications when updating sound. Has to be a better way in gnome. Maybe changing to dunst notifications instead.
 
 line1=$(echo "mpc volume")
-line2=$( sleep 1 && mpc status %volume%)
+line2=$(mpc status %volume%)
 #line2=$(mpc current)
 #line3=$(mpc status %currenttime% %totaltime%%percenttime% | head -n 1)
 
